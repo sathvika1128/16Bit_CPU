3585bc1875670865427a2c521e0746a643435b38
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:36:56 +0530

    Update mux2ne1.v

--- sources/mux2ne1.v
+++ sources/mux2ne1.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/17/2022 02:49:58 PM
-// Design Name: 
-// Module Name: mux2ne1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 module mux2ne1(
     input Hyrja0, //B
