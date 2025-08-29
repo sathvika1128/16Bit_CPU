51bd1daa8d0454fc9b5bab98d41a453417f62993
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:35:58 +0530

    Update SltiFunction.v

--- sources/SltiFunction.v
+++ sources/SltiFunction.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/26/2022 02:44:08 PM
-// Design Name: 
-// Module Name: SltiFunction
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module SltiFunction(
