476d7c2cf8664e0f8fb683e3ca0a7ea17962f4d0
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:33:49 +0530

    Update ALU_Control.v

--- sources/ALU_Control.v
+++ sources/ALU_Control.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/24/2022 08:45:44 PM
-// Design Name: 
-// Module Name: ALU_Control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module ALU_Control(
