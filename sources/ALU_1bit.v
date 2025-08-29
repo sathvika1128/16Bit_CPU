840c355e3a65b0f88430f73cb76535fb94da5405
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:33:28 +0530

    Update ALU_1bit.v

--- sources/ALU_1bit.v
+++ sources/ALU_1bit.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/22/2022 04:41:23 PM
-// Design Name: 
-// Module Name: ALU_1bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 module ALU_1bit(
     input A,
     input B,
