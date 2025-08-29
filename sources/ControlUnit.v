31499e8b590d0539bc421b4ec81e3b80d4bfbeab
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:34:42 +0530

    Update ControlUnit.v

--- sources/ControlUnit.v
+++ sources/ControlUnit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/23/2022 03:53:46 PM
-// Design Name: 
-// Module Name: ControlUnit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ControlUnit(
     input [2:0] OPCODE,
