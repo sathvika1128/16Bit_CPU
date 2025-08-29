f0b2af55f17fbb0b79f472addcea415d2b44f67f
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:35:44 +0530

    Update RegisterFile.v

--- sources/RegisterFile.v
+++ sources/RegisterFile.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/12/2022 05:26:23 PM
-// Design Name: 
-// Module Name: RegisterFile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 module RegisterFile(
 input[2:0] RS,
 input[2:0] RT,
