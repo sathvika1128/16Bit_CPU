a1d1fcf76f53e1ab852d2e4fc668a0f55c20e41c
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:35:29 +0530

    Update InstructionMemory.v

--- sources/InstructionMemory.v
+++ sources/InstructionMemory.v
@@ -1,23 +1,5 @@
 mescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/24/2022 08:35:58 PM
-// Design Name: 
-// Module Name: InstructionMemory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module InstructionMemory(input[15:0] PC, output[15:0] Instruction);
