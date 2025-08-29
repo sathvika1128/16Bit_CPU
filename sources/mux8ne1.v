1bc5fa4a0e584b10bdc6c14c634ec5ab12c758c5
sathvika1128 <160057224+sathvika1128@users.noreply.github.com>
2025-08-29 19:37:11 +0530

    Update mux8ne1.v

--- sources/mux8ne1.v
+++ sources/mux8ne1.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/22/2022 02:52:35 PM
-// Design Name: 
-// Module Name: mux8ne1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module mux8ne1(
